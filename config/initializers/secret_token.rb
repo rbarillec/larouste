8be76abf3b8b506ddbdfe259b8616742afcb21b5
rbarillec <rbarillec@gmail.com>
2012-03-02 10:03:08 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Larouste::Application.config.secret_token = '5406aedd7e844988448169f3c921dc9ac5751cf57925f6fc24607d4f40e817661aff41883417f5924a82864020d6ea803ce3f4fc58bbf153ab05301b013ca241'
